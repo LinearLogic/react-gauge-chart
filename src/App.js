950e28d2af277c100ee5e2f5e5d5c406552578ed
Jared Heath <jared@bellwetherinsight.com>
2023-01-10 15:39:53 -0500

    Formatting

--- src/App.js
+++ src/App.js
@@ -4,23 +4,23 @@ import './App.css'
 import GaugeChart from './lib'
 
 const App = () => {
-  const [currentPercent, setCurrentPercent] = useState();
+  const [currentPercent, setCurrentPercent] = useState()
   const [arcs, setArcs] = useState([0.5, 0.3, 0.2])
 
-	useEffect(() => {
-		const timer = setTimeout(() => {
-      setCurrentPercent(Math.random());
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCurrentPercent(Math.random())
       setArcs([0.1, 0.5, 0.4])
-		}, 3000);
+    }, 3000)
 
-		return () => {
-			clearTimeout(timer);
-		};
-	});
+    return () => {
+      clearTimeout(timer)
+    }
+  })
 
-	const chartStyle = {
-		height: 250,
-	}
+  const chartStyle = {
+    height: 250,
+  }
 
   return (
     <>
@@ -37,20 +37,14 @@ const App = () => {
           </Col>
           <Col xs={12} lg={6}>
             <h6>GaugeChart with 20 levels</h6>
-            <GaugeChart
-							style={chartStyle}
-							nrOfLevels={20}
-							percent={0.86}
-              needleColor="#345243"
-              fontSize="12px"
-						/>
+            <GaugeChart style={chartStyle} nrOfLevels={20} percent={0.86} needleColor='#345243' fontSize='12px' />
           </Col>
         </Row>
         <Row>
           <Col xs={12} lg={6}>
             <h6>GaugeChart with custom colors</h6>
             <GaugeChart
-							style={chartStyle}
+              style={chartStyle}
               nrOfLevels={30}
               colors={['#FF5F6D', '#FFC371']}
               arcWidth={0.3}
@@ -60,21 +54,21 @@ const App = () => {
           <Col xs={12} lg={6}>
             <h6>GaugeChart with larger padding between elements</h6>
             <GaugeChart
-							id="gauge-chart4"
-							style={chartStyle}
-							nrOfLevels={10}
-							arcPadding={0.1}
-							cornerRadius={3}
-							percent={0.6}
-						/>
+              id='gauge-chart4'
+              style={chartStyle}
+              nrOfLevels={10}
+              arcPadding={0.1}
+              cornerRadius={3}
+              percent={0.6}
+            />
           </Col>
         </Row>
         <Row>
           <Col xs={12} lg={6}>
             <h6>GaugeChart with custom arcs width</h6>
             <GaugeChart
-              id="gauge-chart5"
-							style={chartStyle}
+              id='gauge-chart5'
+              style={chartStyle}
               nrOfLevels={420}
               arcsLength={[0.3, 0.5, 0.2]}
               colors={['#5BE12C', '#F5CD19', '#EA4228']}
@@ -85,35 +79,30 @@ const App = () => {
           <Col xs={12} lg={6}>
             <h6>GaugeChart without animation</h6>
             <GaugeChart
-              id="gauge-chart6"
-							style={chartStyle}
+              id='gauge-chart6'
+              style={chartStyle}
               animate={false}
               nrOfLevels={15}
               percent={0.56}
-              needleColor="#345243"
+              needleColor='#345243'
             />
           </Col>
         </Row>
-				<Row>
+        <Row>
           <Col xs={12} lg={6}>
             <h6>GaugeChart with live updates</h6>
-            <GaugeChart
-							id="gauge-chart7"
-							style={chartStyle}
-              percent={currentPercent}
-              animDelay={0}
-            />
+            <GaugeChart id='gauge-chart7' style={chartStyle} percent={currentPercent} animDelay={0} />
           </Col>
           <Col xs={12} lg={6}>
             <h6>GaugeChart with formatted text</h6>
             <GaugeChart
-              id="gauge-chart8"
+              id='gauge-chart8'
               style={chartStyle}
               nrOfLevels={30}
               colors={['#5BE12C', '#F5CD19', '#EA4228']}
               arcWidth={0.3}
               percent={0.37}
-              formatTextValue={value => value + 'kbit/s'}
+              formatTextValue={(value) => value + 'kbit/s'}
             />
           </Col>
         </Row>
@@ -121,8 +110,8 @@ const App = () => {
           <Col xs={12} lg={6}>
             <h6>GaugeChart with arcs update</h6>
             <GaugeChart
-              id="gauge-chart9"
-							style={chartStyle}
+              id='gauge-chart9'
+              style={chartStyle}
               nrOfLevels={420}
               arcsLength={arcs}
               colors={['#5BE12C', '#F5CD19', '#EA4228']}
@@ -130,10 +119,10 @@ const App = () => {
               arcPadding={0.02}
             />
           </Col>
-        </Row>          
+        </Row>
       </Container>
     </>
   )
-};
+}
 
 export default App
